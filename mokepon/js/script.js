8c607ed948db7c32afa1d79a80ebc7ba93b1f522
NahuelZD <nahuelmassa93@gmail.com>
2023-08-02 09:04:26 -0300

    Eleccion de mascotas

--- mokepon/js/script.js
+++ mokepon/js/script.js
@@ -1,4 +1,12 @@
-let botonMascotaJugador = document.getElementById('boton-mascota')
-botonMascotaJugador.addEventListener('click', () => {
-    alert('Seleccionaste una mascota')
+window.addEventListener('load', () => {
+    let botonMascotaJugador = document.getElementById('boton-mascota')
+    botonMascotaJugador.addEventListener('click', () => {
+        if(document.getElementById('hipodoge').checked) alert('Elegiste HIPODOGE')
+        else if(document.getElementById('capipepo').checked) alert('Elegiste CAPIPEPO')
+        else if(document.getElementById('ratigueya').checked) alert('Elegiste RATIGUEYA')
+        else if(document.getElementById('langostelvis').checked) alert('Elegiste LANGOSTELVIS')
+        else if(document.getElementById('tucapalma').checked) alert('Elegiste TUCAPALMA')
+        else if(document.getElementById('pydos').checked) alert('Elegiste PYDOS')
+        else alert('Debes seleccionar una mascota')
+    })
 })
